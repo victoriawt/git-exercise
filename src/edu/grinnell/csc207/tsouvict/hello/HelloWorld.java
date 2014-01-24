8f9ddc500551a72cb9b1b5a8776403f6156507de
victoriawt <vt6666@gmail.com>
2014-01-23 20:43:30 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/tsouvict/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/tsouvict/hello/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
   public static void
   main (String[] args)
 {
-  System.out.println ("Hello, GitHub!");
+  System.out.println ("Hello, my name is victoria!");
 } // main(String[])
 }
