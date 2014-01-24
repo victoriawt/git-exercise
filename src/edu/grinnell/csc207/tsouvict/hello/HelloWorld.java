64aed4083673b5f87d166a0dd534db47873f3684
Victoria <tsouvict@grinnell.edu>
2014-01-23 20:40:50 -0600

    Exercise 8 of Lab1

---
+++ src/edu/grinnell/csc207/tsouvict/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.grinnell.csc207.tsouvict.hello;
+
+public class HelloWorld
+{
+  public static void
+  main (String[] args)
+{
+  System.out.println ("Hello, GitHub!");
+} // main(String[])
+}
